ecb3cc0dc78c8bb0afc7dd3f4f88a69d54b08f41
Mahmoud Bouhlel <mahmoud.bouhlel@episousse.com.tn>
2026-01-21 16:15:37 +0100

    teeeeeeeeed

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -19,6 +19,12 @@ window.Echo = new Echo({
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true
 }); */
+
+import axios from 'axios';
+
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
